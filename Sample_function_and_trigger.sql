a77e3df8bd402cccd4015128d25b5dfe196a94d6
Docker Scheduler <stanislawhorna@outlook.com>
2024-03-17 00:04:28 +0100

    add example function + trigger

---
+++ Sample_function_and_trigger.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION admins_view_insert()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.isActive = true THEN
+        INSERT INTO Admins (user_name, password, isActive, E_mail, Phone_num)
+		VALUES (NEW.user_name, NEW.password, NEW.isActive, NEW.E_mail, NEW.Phone_num);
+		RETURN NULL;
+    ELSE
+        RAISE EXCEPTION 'isActive must be true for insertion';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ioi
+INSTEAD OF INSERT ON admins_view
+FOR EACH ROW
+EXECUTE FUNCTION admins_view_insert();
