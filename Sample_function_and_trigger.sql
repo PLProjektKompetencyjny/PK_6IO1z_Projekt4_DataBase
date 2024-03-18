c109c39aa0260542e1769ce4d033d4dbaf91171b
Docker Scheduler <stanislawhorna@outlook.com>
2024-03-19 00:12:22 +0100

    add RETURN NEW to sample function

--- Sample_function_and_trigger.sql
+++ Sample_function_and_trigger.sql
@@ -4,7 +4,7 @@ BEGIN
     IF NEW.isActive = true THEN
         INSERT INTO Admins (user_name, password, isActive, E_mail, Phone_num)
 		VALUES (NEW.user_name, NEW.password, NEW.isActive, NEW.E_mail, NEW.Phone_num);
-		RETURN NULL;
+		RETURN NEW;
     ELSE
         RAISE EXCEPTION 'isActive must be true for insertion';
     END IF;
