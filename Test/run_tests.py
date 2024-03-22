394b1b0853d33a086590256ed9a3f6ac59579763
Stanislaw Horna <stanislawhorna@outlook.com>
2024-03-22 18:41:30 +0100

    add verification if all test files are configured in Config.json

--- Test/run_tests.py
+++ Test/run_tests.py
@@ -32,6 +32,8 @@ from tabulate import tabulate
 
 TESTS_DIR = "Test"
 CONFIG_FILE_NAME = "Config.json"
+SUCCESS_DIR = "Success"
+FAIL_DIR = "Fail"
 
 # define DB credentials
 DB_USER = "TN_admin"
@@ -41,6 +43,8 @@ DB_PORT = "5432"
 DB_DATABASE = "TravelNest"
 
 # define console text colors
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -54,35 +58,39 @@ class bcolors:
 
 
 def main() -> None:
-    
+
     queries = get_queries_from_file()
     testResults = query_postgres(queries)
     display_results(testResults, queries)
-    
+
     return None
 
+
 def get_queries_from_file() -> list[dict[str, str | bool]]:
-    
+
     # build absolute path to config file
     ConfigFilePath = os.path.join(
         os.path.abspath(os.getcwd()),
         TESTS_DIR,
         CONFIG_FILE_NAME
     )
-        
+
     result = []
-    
+
     # read config
     with open(ConfigFilePath, 'r') as cfg:
         try:
             fileData = "\n".join(cfg.readlines())
         except:
             raise Exception(f"Cannot read config file in path {ConfigFilePath}")
-        
+
         try:
             configuration = json.loads(fileData)
         except:
-            raise Exception("Cannot read JSON config structure, might be corrupted")
+            raise Exception(
+                "Cannot read JSON config structure, might be corrupted")
+
+    checkIfAllExistingFilesAreConfigured(configuration)
 
     # loop through all configured tests
     for file_name in configuration["Tests"]:
@@ -90,27 +98,27 @@ def get_queries_from_file() -> list[dict[str, str | bool]]:
         # build path to test file which will be read
         pathToRead = os.path.join(
             os.path.abspath(os.getcwd()),
-            "Test",
+            TESTS_DIR,
             configuration["QueriesDirectory"],
             file_name
         )
-        
+
         # check if query should end with success or fail
         if (file_name.split("/")[0]) == "Success":
             expectedResult = True
         else:
             expectedResult = False
-        
+
         # extract test name
         testName = file_name.split("/")[1]
-        
+
         # read query from file and append the output
         with open(pathToRead, 'r') as file:
             try:
                 queryString = ("".join(file.readlines()))
             except:
                 raise Exception(f"Cannot read query file in path {pathToRead}")
-            
+
             result.append(
                 {
                     "testName": testName,
@@ -119,13 +127,41 @@ def get_queries_from_file() -> list[dict[str, str | bool]]:
                 }
             )
 
-            
     return result
 
+
+def checkIfAllExistingFilesAreConfigured(configuration) -> None:
+
+    locationToCheck = {
+        'Success': os.path.join(
+            os.path.abspath(os.getcwd()),
+            TESTS_DIR,
+            configuration["QueriesDirectory"],
+            SUCCESS_DIR
+        ),
+        'Fail': os.path.join(
+            os.path.abspath(os.getcwd()),
+            TESTS_DIR,
+            configuration["QueriesDirectory"],
+            FAIL_DIR
+        )
+    }
+
+    configList = [test.split('/')[1] for test in configuration["Tests"]]
+    for type in locationToCheck:
+
+        items = os.listdir(locationToCheck[type])
+        for file in items:
+            if file not in configList:
+                print(bcolors.WARNING + file + " " + type + " test is not configured in config file" +bcolors.ENDC)
+        
+        
+
+
 def query_postgres(queries) -> list[dict[str, str | bool]]:
-    
+
     testResults = []
-    
+
     # loop through configured queries
     for i in range(len(queries)):
         queryResult = None
@@ -140,8 +176,8 @@ def query_postgres(queries) -> list[dict[str, str | bool]]:
             connection = psycopg2.connect(
                 user=DB_USER,
                 password=DB_PASSWORD,
-                host=DB_HOST,  
-                port=DB_PORT,        
+                host=DB_HOST,
+                port=DB_PORT,
                 database=DB_DATABASE
             )
 
@@ -149,70 +185,69 @@ def query_postgres(queries) -> list[dict[str, str | bool]]:
             cursor = connection.cursor()
             cursor.execute(queries[i]["query"])
             connection.commit()
-            
+
             queryResult = True
-            
+
         except Exception as error:
-            
+
             # Assign error to result field
             queryResult = False
             currentTestResult["Error"] = error
         finally:
-            
+
             # fill in appropriate test result
             if queryResult == queries[i]["expectedResult"]:
                 currentTestResult["Result"] = True
             else:
                 currentTestResult["Result"] = False
-            
+
             # append current test result to result list
             testResults.append(currentTestResult)
-            
+
             # close connection
             if connection:
                 cursor.close()
                 connection.close()
 
-            
     return testResults
 
 
 def display_results(testResults, queries):
-    
+
     # set table headers
-    dataHeaders = ["Test name", "Test type","Successful"]
-    
+    dataHeaders = ["Test name", "Test type", "Successful"]
+
     # set table data
     data = []
-    
+
     for i in range(len(testResults)):
         if queries[i]["expectedResult"] == True:
             testType = "Success"
         else:
             testType = "Fail"
         data.append(
-            [testResults[i]["Name"],testType, testResults[i]["Result"]]
+            [testResults[i]["Name"], testType, testResults[i]["Result"]]
         )
-        
-    
+
     # print result table
     print()
     print(
         tabulate(
-            headers=dataHeaders, 
-            tabular_data=data, 
+            headers=dataHeaders,
+            tabular_data=data,
             tablefmt="github",
         )
     )
     print()
-    
-    # print errors for tests which ended different than expected 
+
+    # print errors for tests which ended different than expected
     for test in testResults:
         if test["Result"] != True:
             print(bcolors.FAIL + test["Name"] + bcolors.ENDC)
             print(test["Error"])
-    
+
     return None
+
 
 if __name__ == "__main__":
     main()
