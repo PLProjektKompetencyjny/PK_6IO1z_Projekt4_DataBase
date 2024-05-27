879b65f75d94782da9be5de172e79d6023ba08aa
Stanislaw Horna <stanislawhorna@outlook.com>
2024-05-27 22:57:39 +0200

    add initial data

---
+++ SQL/10.Insert_initial_data.sql
@@ -0,0 +1,248 @@
+/*
+Insert initial data
+*/
+SELECT
+	INSERT_USER_ACCOUNT ('john.doe@wp.pl', 'myjohn', NULL);
+
+SELECT
+	INSERT_USER_ACCOUNT ('kevin.smith@wp.pl', 'mykevin', NULL);
+
+SELECT
+	INSERT_USER_ACCOUNT ('jack.connor@wp.pl', 'myjack', NULL);
+
+INSERT INTO
+	USER_DETAILS (
+		USER_ID,
+		NIP_NUM,
+		NAME,
+		SURNAME,
+		PHONE_NUM,
+		CITY,
+		POSTAL_CODE,
+		STREET,
+		BUILDING_NUM
+	)
+VALUES
+	(
+		1,
+		NULL,
+		'John',
+		'Doe',
+		'+44123456789',
+		'London',
+		'12-324',
+		'Hampson',
+		'12'
+	),
+	(
+		2,
+		NULL,
+		'Kevin',
+		'Smith',
+		'+44123456789',
+		'Birmingham',
+		'10-944',
+		'Wellington',
+		'22'
+	),
+	(
+		3,
+		NULL,
+		'Jack',
+		'Connor',
+		'+44123456789',
+		'Dudley',
+		'10-881',
+		'Brooke',
+		'33'
+	);
+
+DO $$
+DECLARE
+	user_id_var int;
+BEGIN
+	SELECT
+		insert_user_account('ADMIN','mypass', NULL)
+	INTO user_id_var;
+
+	UPDATE user_view
+	SET user_is_admin = TRUE
+	WHERE user_id = user_id_var;
+	
+END $$;
+
+INSERT INTO
+	ROOM_TYPE (
+		NUM_OF_SINGLE_BEDS,
+		NUM_OF_DOUBLE_BEDS,
+		NUM_OF_CHILD_BEDS,
+		ADULT_PRICE_GROSS,
+		CHILD_PRICE_GROSS,
+		PHOTOS_DIR
+	)
+VALUES
+	(0, 1, 1, 20, 10, '/type_1'),
+	(1, 2, 3, 5, 0, '/type_2');
+
+INSERT INTO
+	ROOM (ID, ROOM_TYPE_ID, ROOM_PRICE_GROSS)
+VALUES
+	(1, 1, 10),
+	(2, 2, 20),
+	(3, 1, 30),
+	(4, 2, 40);
+
+INSERT INTO
+	SERVICE (NAME, UNIT_PRICE)
+VALUES
+	('Parking', 25),
+	('Breakfast', 30),
+	('Dinner', 50),
+	('Swimming pool', 100),
+	('Champagne', 300);
+
+
+INSERT INTO
+	RESERVATION_VIEW (
+		RESERVATION_CUSTOMER_ID,
+		ROOM_NUMBER_OF_ADULTS,
+		ROOM_NUMBER_OF_CHILDREN,
+		RESERVATION_START_DATE,
+		RESERVATION_END_DATE,
+		RESERVATION_ROOM_ID
+	)
+VALUES
+	(1, 1, 1, (current_date + 10), (current_date + 15), 1);
+
+INSERT INTO
+	SERVICE_VIEW (
+		SERVICE_RESERVATION_ID,
+		SERVICE_ID,
+		SERVICE_QUANTITY
+	)
+VALUES
+	(1,1,3),
+	(1,2,5),
+	(1,4,3);
+
+INSERT INTO
+	RESERVATION_VIEW (
+		RESERVATION_CUSTOMER_ID,
+		ROOM_NUMBER_OF_ADULTS,
+		ROOM_NUMBER_OF_CHILDREN,
+		RESERVATION_START_DATE,
+		RESERVATION_END_DATE,
+		RESERVATION_ROOM_ID
+	)
+VALUES
+	(2, 1, 1, (current_date + 1), (current_date + 5), 1),
+	(2, 1, 1, (current_date + 1), (current_date + 5), 2);
+
+INSERT INTO
+	SERVICE_VIEW (
+		SERVICE_RESERVATION_ID,
+		SERVICE_ID,
+		SERVICE_QUANTITY
+	)
+VALUES
+	(2,2,3),
+	(2,5,5);
+
+INSERT INTO
+	RESERVATION_VIEW (
+		RESERVATION_CUSTOMER_ID,
+		ROOM_NUMBER_OF_ADULTS,
+		ROOM_NUMBER_OF_CHILDREN,
+		RESERVATION_START_DATE,
+		RESERVATION_END_DATE,
+		RESERVATION_ROOM_ID
+	)
+VALUES
+	(3, 1, 1, (current_date + 20), (current_date + 30), 1),
+	(3, 1, 1, (current_date + 20), (current_date + 30), 2),
+	(3, 1, 1, (current_date + 20), (current_date + 30), 3);
+
+INSERT INTO
+	SERVICE_VIEW (
+		SERVICE_RESERVATION_ID,
+		SERVICE_ID,
+		SERVICE_QUANTITY
+	)
+VALUES
+	(3,1,1),
+	(3,2,2),
+	(3,3,3),
+	(3,4,4),
+	(3,5,5);
+
+INSERT INTO
+	RESERVATION_VIEW (
+		RESERVATION_CUSTOMER_ID,
+		ROOM_NUMBER_OF_ADULTS,
+		ROOM_NUMBER_OF_CHILDREN,
+		RESERVATION_START_DATE,
+		RESERVATION_END_DATE,
+		RESERVATION_ROOM_ID
+	)
+VALUES
+	(3, 1, 1, (current_date  + 1), (current_date + 2), 1);
+
+INSERT INTO
+	SERVICE_VIEW (
+		SERVICE_RESERVATION_ID,
+		SERVICE_ID,
+		SERVICE_QUANTITY
+	)
+VALUES
+	(4,1,10);
+
+INSERT INTO
+	RESERVATION_VIEW (
+		RESERVATION_CUSTOMER_ID,
+		ROOM_NUMBER_OF_ADULTS,
+		ROOM_NUMBER_OF_CHILDREN,
+		RESERVATION_START_DATE,
+		RESERVATION_END_DATE,
+		RESERVATION_ROOM_ID
+	)
+VALUES
+	(3, 1, 1, (current_date + 3), (current_date + 5), 1),
+	(3, 1, 1, (current_date + 3), (current_date + 5), 2);
+
+INSERT INTO
+	SERVICE_VIEW (
+		SERVICE_RESERVATION_ID,
+		SERVICE_ID,
+		SERVICE_QUANTITY
+	)
+VALUES
+	(5,2,20),
+    (5,3,20);
+
+INSERT INTO
+	RESERVATION_VIEW (
+		RESERVATION_CUSTOMER_ID,
+		ROOM_NUMBER_OF_ADULTS,
+		ROOM_NUMBER_OF_CHILDREN,
+		RESERVATION_START_DATE,
+		RESERVATION_END_DATE,
+		RESERVATION_ROOM_ID
+	)
+VALUES
+	(1, 1, 1, (current_date + 100), (current_date + 120), 1),
+	(1, 1, 1, (current_date + 100), (current_date + 120), 2),
+	(1, 1, 1, (current_date + 100), (current_date + 120), 3);
+
+INSERT INTO
+	INVOICE_VIEW (INVOICE_RESERVATION_ID)
+VALUES
+	(1),
+	(2),
+	(3),
+	(6);
+
+UPDATE INVOICE_VIEW
+SET
+	INVOICE_IS_PAID = TRUE
+WHERE
+	INVOICE_ID IN (2, 1, 4);
