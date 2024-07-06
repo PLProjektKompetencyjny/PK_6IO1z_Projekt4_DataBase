ea9877795186476816e35e6c30bed463b524672a
Stanislaw Horna <stanislawhorna@outlook.com>
2024-07-06 14:47:36 +0200

    fix/Re-calculate invoice price on room delete

--- SQL/05.CreateDeleteFunctions.sql
+++ SQL/05.CreateDeleteFunctions.sql
@@ -74,13 +74,6 @@ BEGIN
         ROOM_ID = OLD.RESERVATION_ROOM_ID
         AND RESERVATION_ID = OLD.RESERVATION_ID;
 
-    DELETE FROM INVOICE_VIEW
-    WHERE
-        invoice_reservation_id = OLD.RESERVATION_ID
-        AND invoice_room_id = OLD.RESERVATION_ROOM_ID;
-
-    PERFORM calculate_invoice_price(OLD.RESERVATION_ID);
-
     IF NOT EXISTS (
         SELECT
             ROOM_ID
@@ -95,7 +88,8 @@ BEGIN
 
         DELETE FROM RESERVATION
         WHERE ID = OLD.RESERVATION_ID;
-
+    ELSE
+        PERFORM calculate_invoice_price(OLD.RESERVATION_ID);
     END IF;
 
 	RETURN NULL;
